47554a761c929ab23a6b8e0bc91b3188b79433eb
Fredrik Johansson <fjohansson.creative@gmail.com>
2026-05-10 19:29:02 +0200

    fix deploy

--- src/deploy.ts
+++ src/deploy.ts
@@ -3,14 +3,17 @@ import { REST, Routes } from "discord.js"
 import { statsCommand } from "./api/pubg/commands/stats"
 import { leaderboardCommand } from "./api/pubg/commands/leaderboard"
 
-const rest = new REST().setToken(process.env.DISCORD_BOT_TOKEN_DEV!)
-
 const isDev = !!process.env.DISCORD_GUILD_ID
+
+const token = isDev ? process.env.DISCORD_BOT_TOKEN_DEV! : process.env.DISCORD_BOT_TOKEN!
+const clientId = isDev ? process.env.DISCORD_BOT_CLIENT_ID_DEV! : process.env.DISCORD_BOT_CLIENT_ID!
+
+const rest = new REST().setToken(token)
 
 await rest.put(
   isDev
-    ? Routes.applicationGuildCommands(process.env.DISCORD_BOT_CLIENT_ID_DEV!, process.env.DISCORD_GUILD_ID!)
-    : Routes.applicationCommands(process.env.DISCORD_BOT_CLIENT_ID_DEV!),
+    ? Routes.applicationGuildCommands(clientId, process.env.DISCORD_GUILD_ID!)
+    : Routes.applicationCommands(clientId),
   { body: [statsCommand.toJSON(), leaderboardCommand.toJSON()] }
 )
 
